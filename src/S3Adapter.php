c42f81032cc106a5e87f3fcdc8a35a6424391577
Leon <leongrdic@gmail.com>
2022-03-07 16:06:44 +0100

    initial

---
+++ src/S3Adapter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Le\S3Adapter;
+
+use Aws\S3\{S3Client, S3ClientInterface};
+
+class S3Adapter
+{
+
+    public function __construct(
+        public string             $bucket,
+        string                    $region,
+        string                    $key,
+        string                    $secret,
+        string                    $endpoint = '',
+        string                    $version = 'latest',
+        public ?S3ClientInterface $s3Client = null
+    )
+    {
+        $this->s3Client ??= new S3Client([
+            'region' => $region,
+            'version' => $version,
+            'endpoint' => $endpoint,
+            'credentials' => [
+                'key' => $key,
+                'secret' => $secret
+            ]
+        ]);
+    }
+
+    public function put(string $key, string $data)
+    {
+        return $this->s3Client->putObject([
+            'Bucket' => $this->bucket,
+            'Key' => $key,
+            'Body' => $data,
+        ]);
+    }
+
+    public function get(string $key, bool $justBody = false)
+    {
+        $result = $this->s3Client->getObject([
+            'Bucket' => $this->bucket,
+            'Key' => $key,
+        ]);
+
+        return $justBody ? $result['Body'] : $result;
+    }
+
+    public function delete(string $key)
+    {
+        return $this->s3Client->deleteObject(array(
+            'Bucket' => $this->bucket,
+            'Key'    => $key,
+        ));
+    }
+
+}
